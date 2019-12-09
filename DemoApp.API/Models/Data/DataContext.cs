212ca4f49aed885c4d2bd610af8c56061d3728b3
Neha Gawri <neha.gawri@tetrapak.com>
2019-12-09 22:26:49 +0530

    initial setup

---
+++ DemoApp.API/Models/Data/DataContext.cs
@@ -0,0 +1,11 @@
+namespace DemoApp.API.Models.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    public class DataContext:DbContext
+    
+    {
+            public DataContext (DbContextOptions<DataContext> options) : base (options) { }
+            public DbSet<Value> Values { get; set; }
+        }
+    }
+   
